217160d0e6acfe6f0ab7d6a2cc63c87bcff9eaf2
Tabitha <tabithaprojects@outlook.com>
2026-08-17 23:24:38 -0700

    Remediate storage account security misconfigurations

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
-# 1. Tell Terraform to use Microsoft Azure as our cloud provider
 terraform {
-  required_version = ">= 1.5.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -13,30 +11,34 @@ provider "azurerm" {
   features {}
 }
 
-# 2. Define an Azure Resource Group (a container for cloud resources)
 resource "azurerm_resource_group" "rg" {
   name     = "rg-devsecops-demo"
   location = "East US"
-
-  tags = {
-    Environment = "Dev"
-    ManagedBy   = "Terraform"
-  }
 }
 
-# 3. Define an Azure Storage Account (with INTENTIONAL misconfigurations for our security scanner)
-resource "azurerm_storage_account" "sa" {
+resource "azurerm_storage_account" "secure_storage" {
   name                     = "stdevsecopsdemo001"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 
-  # SECURITY VULNERABILITIES FOR TESTING:
-  # - Outdated TLS version
-  # - Allows public access to files
-  allow_nested_items_to_be_public = true
-  min_tls_version                 = "TLS1_0"
+  # Security Hardening Controls
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+  public_network_access_enabled   = false
 
-tags = { Environment = "Dev" }
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+    container_delete_retention_policy {
+      days = 7
+    }
+  }
+
+  tags = {
+    Environment = "DevSecOps"
+    ManagedBy   = "Terraform"
+  }
 }
